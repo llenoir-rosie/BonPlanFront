e9d1537bdd7318de74a3495690dae24fecc5e78d
Thibault FERNEY <tferney@consultants-solutec.fr>
2023-03-16 17:51:23 +0100

    derniere version jeudi

---
+++ src/app/border-card.directive.ts
@@ -0,0 +1,27 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[testBorderCard]'
+})
+export class BorderCardDirective {
+
+  constructor(private el: ElementRef) { 
+    this.setHeight(450);
+    this.setBorder('#f5f5f5');
+  }
+  @HostListener('mouseenter') onMouseEnter() {
+    this.setBorder('#009688');
+  
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.setBorder('#f5f5f5');
+  } 
+  setHeight(height: number) {
+    this.el.nativeElement.style.height = `${height}px` ;
+  }
+
+  setBorder(color: string) {
+    this.el.nativeElement.style.border = `solid 4px ${color}` ;
+  }
+
+}
