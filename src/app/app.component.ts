8bbf65e522a0f4bc447e489e0d8800df2232d9e8
Thibault FERNEY <tferney@consultants-solutec.fr>
2023-03-16 11:44:16 +0100

    pour gab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Ville } from './ville';
-import { VILLE } from './mock-ville-list';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styles: []
 })
-export class AppComponent implements OnInit {
-  listVille: Ville[] = VILLE;
-
-  ngOnInit() {
-    console.table(this.listVille)
-  }
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListeVilleComponent } from './liste-ville/liste-ville.component';
+import { ListeActiviteComponent } from './liste-activite/liste-activite.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListeVilleComponent,
+    ListeActiviteComponent
   ],
   imports: [
     BrowserModule,
