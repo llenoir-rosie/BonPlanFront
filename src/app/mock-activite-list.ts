0c8c0586e7c347f7804f40b59a9edf6b6599dd0e
Thibault FERNEY <tferney@consultants-solutec.fr>
2023-03-17 14:48:51 +0100

    apres gab

---
+++ src/app/mock-activite-list.ts
@@ -0,0 +1,34 @@
+import { Activite } from "./activite";
+
+export const ACTIVITE: Activite[] = [
+    {
+        nameville:"Grenoble",
+        name:"Ski",
+        description:"Permet de descendre des pentes rapidement"
+    },
+    {
+        nameville:"Grenoble",
+        name:"Randonnée",
+        description:"Permet de faire des ballades en montagne"
+    },
+    {
+        nameville:"Grenoble",
+        name:"Course",
+        description:"pour s'entraîner au marathon"
+    },
+    {
+        nameville:"Lyon",
+        name:"Course",
+        description:"pour s'entraîner au marathon"
+    },
+    {
+        nameville:"Lyon",
+        name:"Piscine",
+        description:"Pour entraîner son cardio"
+    },
+    {
+        nameville:"Marseille",
+        name:"Course",
+        description:"pour s'entraîner au marathon"
+    }
+]
