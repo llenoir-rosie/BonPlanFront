d85fd07bdeab9144dcb2f0e725e9f38b0349da7a
Thibault FERNEY <tferney@consultants-solutec.fr>
2023-03-17 09:44:47 +0100

    test d envoie des ville

--- src/app/liste-ville/liste-ville.component.ts
+++ src/app/liste-ville/liste-ville.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { VILLE } from '../mock-ville-list';
 import { Ville } from '../ville';
 
@@ -8,4 +9,11 @@ import { Ville } from '../ville';
 })
 export class ListeVilleComponent {
   listVille: Ville[] = VILLE;
+
+  constructor(private router: Router) {}
+   
+  goToVilleActivite(ville: Ville) {
+      this.router.navigate(['/ville', ville.name])
+    }
+  
 }
