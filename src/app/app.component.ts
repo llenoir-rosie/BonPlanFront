c205df3f1967309547b529a73ebdba14bb22672e
Gabriel PONS <gpons@consultants-solutec.fr>
2023-03-20 17:54:47 +0100

    début de chose avec le bouton recherche

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { debounceTime, distinctUntilChanged, Observable, Subject, switchMap } from 'rxjs';
+import { VILLE } from './mock-ville-list';
+import { Ville } from './ville';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
 })
-export class AppComponent {}
+
+export class AppComponent implements OnInit {
+  searchTerms = new Subject<string>();
+  Villeslist: Ville[] = VILLE;
+
+  constructor(private router: Router) { }
+
+  goToVilleActivite(ville: Ville) {
+    this.router.navigate(['/ville', ville.name])
+  }
+
+  ngOnInit(): void {
+    // this.Villes$ = this.searchTerms.pipe(
+    //   debounceTime(300),
+    //   distinctUntilChanged(),
+    //   switchMap((term) => this.searchVilleList(term))
+    // );
+    console.log(this.Villeslist)
+
+  }
+
+  search(term: string) {
+    this.searchTerms.next(term);
+  }
+
+  goToDetail(Ville: Ville) {
+    const link = ['/Ville', Ville.name];
+    this.router.navigate(link);
+  }
+
+}
 
