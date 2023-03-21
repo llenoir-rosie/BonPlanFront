0a2ce6a624f30e73396d557f92d17dc25f5d590a
Thibault FERNEY <tferney@consultants-solutec.fr>
2023-03-21 11:37:23 +0100

    Bon jusquau bonplan

--- src/app/list-bonplan/list-bonplan.component.ts
+++ src/app/list-bonplan/list-bonplan.component.ts
@@ -36,25 +36,26 @@ export class ListBonplanComponent implements OnInit {
     this.activiteList = ACTIVITE;
 
     const villeName: string|null = this.route.snapshot.paramMap.get('ville.name');
+    const activiteName: string|null = this.route.snapshot.paramMap.get('act.name');
     let nomdelaville = (villeName+'').charAt(0).toUpperCase()+villeName?.substr(1)
-
-    this.ville = this.villeList.find(ville => ville.name == nomdelaville);
-
-    const activiteName: string|null = this.route.snapshot.paramMap.get('activite.name');
     let nomdelactivite = (activiteName+'').charAt(0).toUpperCase()+activiteName?.substr(1)
 
-
-    this.activite = this.activiteList.find(activite => activite.name == nomdelactivite)
+    this.ville = this.villeList.find(ville => ville.name == nomdelaville);
+    this.activite = this.activiteList.find(activite => activite.nameville == nomdelaville)
+    
+    
     this.bpList.forEach(element => {
-      if (element.nameville,element.nameactivite === nomdelaville,nomdelactivite){
+      if (element.nameville === nomdelaville && element.nameactivite === nomdelactivite){
         this.bp.push(element)
 
   }});
   console.log(activiteName)
-  console.log(nomdelactivite)
+  console.log(this.activite)
+
   console.log(this.bp)
-  console.log(this.villeList)
   console.log(this.bpList)
+
+
 }
   goToVillePrecision(ville: Ville, act: Activite, bp: Bonplan) {
     this.router.navigate(['/ville', ville.name, act.name, bp.name])
