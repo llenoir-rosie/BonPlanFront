e5c2e6d0c1b99298f9ae82c9b7ac5aa86e82461f
Gabriel PONS <gpons@consultants-solutec.fr>
2023-05-04 17:21:43 +0200

    ajout des pages d'erreurs

--- src/app/list-bonplan/list-bonplan.component.ts
+++ src/app/list-bonplan/list-bonplan.component.ts
@@ -46,6 +46,10 @@ throw new Error('Method not implemented.');
   currentVille: String;
   currentActivite: String;
   newBP: Bonplan;
+  public listActivities: Activite[];
+  public listNameActivities: String[] = [];
+  public listVille: Ville[];
+  public listNameVille: String[] = [];
   
   allBonPlan: BonPlanNote[];
   allMauvaisPlan: BonPlanNote[];
@@ -83,6 +87,9 @@ throw new Error('Method not implemented.');
 
     localStorage.setItem("currentVille"," à " + this.nomdelaville.toString());
     localStorage.setItem("currentActivite", "\xa0"  + this.nomdelactivite.toString());
+
+    this.getAllActivityName();
+    this.getAllVilleName();
   }
 
 
@@ -270,4 +277,21 @@ throw new Error('Method not implemented.');
     return [Math.round(delta_final) , Unite]
   }
   
+  getAllActivityName(){
+    this.http.get<Activite[]>('http://localhost:8080/activities').subscribe((data) => {
+      this.listActivities = data;
+      for (let n: number = 0; n < this.listActivities.length; n++){
+        this.listNameActivities.push(this.listActivities[n].name);
+      }
+    })
+  }
+  getAllVilleName(){
+    this.http.get<Ville[]>('http://localhost:8080/cities').subscribe((data) => {
+      this.listVille = data;
+      for (let n: number = 0; n < this.listVille.length; n++){
+        this.listNameVille.push(this.listVille[n].name);
+      }
+    })
+  }
+
 }
